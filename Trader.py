fecf2952c36989f66840ab0687c966f34fcd71bd
Reza82-python <mohammadreza.kamali9120@gmail.com>
2025-02-26 10:26:36 +0330

    get 200candle price

--- Trader.py
+++ Trader.py
@@ -1,33 +1,19 @@
-import requests
+from pybit.unified_trading import HTTP
+import pandas as pd
 import time
-import hmac
-import hashlib
 
-# ---- اطلاعات API ----
-api_key = "GVEK8T68lnSAcCZ16L"
-api_secret = "XYLzLl7h8feWWJ7Mved9vAncZSarJ6RfB1mC"
+# اتصال به Bybit (در حالت تست‌نت)
+url = HTTP(testnet=True)
+# گرفتن اطلاعات آخرین قیمت
+response = url.get_index_price_kline(
+    category="linear",  # نوع قرارداد (linear برای فیوچرز)
+    symbol="BTCUSDT",  # نماد بازار (BTC/USDT)
+    interval=240,  # تایم‌فریم (60 دقیقه)
+    limit=200,  # تعداد داده‌ها، فقط یک کندل آخر
+    start=int(time.time()) - 86400,
+)
+if 'result' in response:
+    kline_data = response['result']
 
-# ---- تایم‌استمپ (برای احراز هویت) ----
-timestamp = str(int(time.time() * 1000))
-
-# ---- پارامترهای درخواست ----
-params = {
-    "symbol": "BTCUSDT"
-}
-
-# ---- ساختن امضا برای درخواست ----
-query_string = "&".join([f"{key}={value}" for key, value in params.items()])
-signature = hmac.new(api_secret.encode(), query_string.encode(), hashlib.sha256).hexdigest()
-
-# ---- ارسال درخواست ----
-url = f"https://api.bybit.com/v5/market/tickers?{query_string}&api_key={api_key}&timestamp={timestamp}&sign={signature}"
-
-response = requests.get(url)
-data = response.json()
-
-# ---- بررسی نتیجه ----
-if "result" in data:
-    btc_price = data["result"]["list"][0]["lastPrice"]
-    print(f"📌 قیمت بیت‌کوین: {btc_price} USDT")
-else:
-    print("❌ خطا در دریافت اطلاعات", data)
+    df = pd.DataFrame(kline_data)
+    print(df.head())
